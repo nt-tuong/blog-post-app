a5abca73c3a296c22fcee35baf43e10a82511100
tuongthanh98 <tuongthanh98@gmail.com>
2025-10-23 20:15:51 +0700

    timeout

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,16 +1,18 @@
 import React, { useRef, useEffect } from "react";
 
 function Input(props) {
-  const refInput = useRef();
+  const inputRef = useRef();
   useEffect(()=>{
-    if (refInput.current) {
-      refInput.current.focus();
-    }
+    const timer = setTimeout(() => {
+      inputRef.current?.focus();
+    }, 300); // Adjust delay as needed
+
+    return () => clearTimeout(timer);
   },[])
 
   return (
     <div className="layout-column justify-content-center align-items-center">
-      <input ref={refInput} className="w-100" type="text" placeholder="Enter Title" value={props.blogPost?.title} name="title" data-testid="title-input" onChange={props.handleChange} />
+      <input ref={inputRef} className="w-100" type="text" placeholder="Enter Title" value={props.blogPost?.title} name="title" data-testid="title-input" onChange={props.handleChange} />
       <textarea className="mt-10 w-100" placeholder="Enter Description" value={props.blogPost?.description} name="description" data-testid="description-input" onChange={props.handleChange} />
     </div>
   );
